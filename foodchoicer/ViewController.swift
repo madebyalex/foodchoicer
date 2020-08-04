1fc6f94a0fc596111e71843fc57e34bdd66bb5ee
Alexander <hello@alexander.works>
2020-08-04 22:14:34 +0300

    Set up general structure and implemented the basic logic

--- foodchoicer/ViewController.swift
+++ foodchoicer/ViewController.swift
@@ -9,7 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let burgerTime = 15
+    let hotdogTime = 12
+    let soupTime = 30
+    let pizzaTime = 25
+    let tacoTime = 40
+    let cocktailTime = 5
+    
+    @IBAction func mealSelected(_ sender: UIButton) {
+        let meal = sender.currentTitle!.lowercased()
+        
+        switch meal {
+            case "burger":
+                print(burgerTime)
+            case "hotdog":
+                print(hotdogTime)
+            case "soup":
+                print(soupTime)
+            case "pizza":
+                print(pizzaTime)
+            case "taco":
+                print(tacoTime)
+            case "cocktail":
+                print(cocktailTime)
+            default:
+                print("There is no such meal, sorry")
+        }
+        
+//        let timeName: String = meal + "Time"
 
+//        let varName = NSPredicate(format: "%@", timeName)
+        
+//        if (String(varName) == timeName) {
+//            print(meal)
+//        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
