e81510842c8b5bf765a8130a49674dc62b3de9cd
Alexander <hello@alexander.works>
2020-08-05 03:22:20 +0300

    Changing the text content during the timer's countdowns

--- foodchoicer/ViewController.swift
+++ foodchoicer/ViewController.swift
@@ -9,8 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var headline: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
     
-    let mealTimes = ["burger": 300, "hotdog": 420, "soup": 1200, "pizza": 900, "taco": 1500, "cocktail": 720]
+    
+    
+//    let mealTimes = ["burger": 300, "hotdog": 420, "soup": 1200, "pizza": 900, "taco": 1500, "cocktail": 720]
+    
+    let mealTimes = ["burger": 3, "hotdog": 4, "soup": 5, "pizza": 7, "taco": 6, "cocktail": 8]
     
     var secondsRemaining = 60
     var timer = Timer()
@@ -18,17 +24,24 @@ class ViewController: UIViewController {
     @IBAction func mealSelected(_ sender: UIButton) {
         
         timer.invalidate()
+        headline.text = "Cooking... ⏱"
         
         let meal = sender.currentTitle!.lowercased()
         secondsRemaining = mealTimes[meal]!
+        
+        descriptionLbl.text = "Your order is accepted."
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
         if secondsRemaining > 0 {
-            print("Remaining \(secondsRemaining) seconds")
+//            print("Remaining \(secondsRemaining) seconds")
+            descriptionLbl.text = "Please wait \(secondsRemaining) seconds"
             secondsRemaining -= 1
+        } else {
+            headline.text = "Yay! 🙌"
+            descriptionLbl.text = "Enjoy your meal."
         }
     }
     
